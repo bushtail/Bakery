4556e5737e1cc575afa4a822986a153e82e8c06b
Evan Nosich <evannosich@icloud.com>
2019-11-23 14:58:38 -0500

    Added smoker functionality.

--- src/ca/evannosich/Bakery/Bakery.java
+++ src/ca/evannosich/Bakery/Bakery.java
@@ -5,6 +5,7 @@ import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.FurnaceRecipe;
+import org.bukkit.inventory.SmokingRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Bakery extends JavaPlugin {
@@ -12,17 +13,29 @@ public class Bakery extends JavaPlugin {
     @Override
     public void onEnable() {
 
+
+
         ItemStack breadItem = new ItemStack(Material.BREAD);
 
         NamespacedKey breadKey = new NamespacedKey(this, "BREAD");
 
         float experience = 0.35f;
 
-        int cookingTime = 200;
+        // Furnace
 
-        FurnaceRecipe breadRecipe = new FurnaceRecipe(breadKey, breadItem, Material.WHEAT, experience, cookingTime);
+        int cookingTimeFurnace = 200;
 
-        Bukkit.addRecipe(breadRecipe);
+        FurnaceRecipe breadRecipeFurnace = new FurnaceRecipe(breadKey, breadItem, Material.WHEAT, experience, cookingTimeFurnace);
+
+        Bukkit.addRecipe(breadRecipeFurnace);
+
+        // Smoker
+
+        int cookingTimeSmoker = 100;
+
+        SmokingRecipe breadRecipeSmoker = new SmokingRecipe(breadKey, breadItem, Material.WHEAT, experience, cookingTimeSmoker);
+
+        Bukkit.addRecipe(breadRecipeSmoker);
 
     }
 
