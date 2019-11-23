1857a02bafc32d72e22dbdeee282cb1079dd8ba9
Evan Nosich <evannosich@icloud.com>
2019-11-23 14:46:59 -0500

    Coded plugin, added outfile.

--- src/ca/evannosich/Bakery/Bakery.java
+++ src/ca/evannosich/Bakery/Bakery.java
@@ -1,5 +1,34 @@
 package ca.evannosich.Bakery;
 
-public class Bakery {
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.FurnaceRecipe;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Bakery extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        ItemStack breadItem = new ItemStack(Material.BREAD);
+
+        NamespacedKey breadKey = new NamespacedKey(this, "BREAD");
+
+        float experience = 0.35f;
+
+        int cookingTime = 200;
+
+        FurnaceRecipe breadRecipe = new FurnaceRecipe(breadKey, breadItem, Material.WHEAT, experience, cookingTime);
+
+        Bukkit.addRecipe(breadRecipe);
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
 
 }
